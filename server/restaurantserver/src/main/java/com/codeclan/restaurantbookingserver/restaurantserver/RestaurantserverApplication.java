08e956f935fa2cf38052f23e0d5247695379f014
Paul Wishart <misterwishart@gmail.com>
2019-02-01 14:31:38 +0000

    Initialised React client and Spring server

---
+++ server/restaurantserver/src/main/java/com/codeclan/restaurantbookingserver/restaurantserver/RestaurantserverApplication.java
@@ -0,0 +1,14 @@
+package com.codeclan.restaurantbookingserver.restaurantserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantserverApplication.class, args);
+	}
+
+}
+
