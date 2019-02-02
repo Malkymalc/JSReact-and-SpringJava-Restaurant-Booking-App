5cad144ef0abdd6c9613c5cc3aab23673a38d6d5
Ernest Fraiman <upapaus@gmail.com>
2019-02-02 14:52:23 +0000

    create routes for bookings by date and by customer

--- server/restaurantserver/src/main/java/com/codeclan/restaurantbookingserver/restaurantserver/controllers/BookingController.java
+++ server/restaurantserver/src/main/java/com/codeclan/restaurantbookingserver/restaurantserver/controllers/BookingController.java
@@ -1,13 +1,35 @@
 package com.codeclan.restaurantbookingserver.restaurantserver.controllers;
 
 
+import com.codeclan.restaurantbookingserver.restaurantserver.models.Booking;
+import com.codeclan.restaurantbookingserver.restaurantserver.repositories.bookingRepository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.List;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/bookings")
 public class BookingController {
+
     @Autowired
-    BookingController bookingController;
+    BookingRepository bookingRepository;
+
+    @GetMapping("date/{date}")
+    //@DateTimeFormat(pattern = "dd-MM-yy")
+    public List<Booking> getAllBookingsByDate(@PathVariable @DateTimeFormat(pattern="yyyy-MM-dd") Date date){
+        return bookingRepository.findBookingsByDate(date);
+    }
+
+    @GetMapping("bycustomer/{customerId}")
+    public List<Booking> getAllBookingsByCustomerId(@PathVariable Long customerId){
+        return bookingRepository.findBookingsByCustomerId(customerId);
+
+    }
+
 }
--- server/restaurantserver/src/main/java/com/codeclan/restaurantbookingserver/restaurantserver/repositories/bookingRepository/BookingRepository.java
+++ server/restaurantserver/src/main/java/com/codeclan/restaurantbookingserver/restaurantserver/repositories/bookingRepository/BookingRepository.java
@@ -2,6 +2,14 @@ package com.codeclan.restaurantbookingserver.restaurantserver.repositories.booki
 
 import com.codeclan.restaurantbookingserver.restaurantserver.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findBookingsByDate(Date date);
+    List<Booking> findBookingsByCustomerId(Long customerId);
+
 }
