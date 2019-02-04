d86f612da9f4f766b2b2928f78a51575f42b0984
Paul Wishart <misterwishart@gmail.com>
2019-02-04 16:38:40 +0000

    Got Embed Customer working for front end

--- server/restaurantserver/src/main/java/com/codeclan/restaurantbookingserver/restaurantserver/projections/EmbedCustomer.java
+++ server/restaurantserver/src/main/java/com/codeclan/restaurantbookingserver/restaurantserver/projections/EmbedCustomer.java
@@ -2,10 +2,17 @@ package com.codeclan.restaurantbookingserver.restaurantserver.projections;
 
 import com.codeclan.restaurantbookingserver.restaurantserver.models.Booking;
 import com.codeclan.restaurantbookingserver.restaurantserver.models.Customer;
+import com.codeclan.restaurantbookingserver.restaurantserver.models.Table;
 import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
 
 @Projection(name= "embedCustomer", types = Booking.class)
 
 public interface EmbedCustomer {
+    String getDate();
+    String getTime();
     Customer getCustomer();
+    int getHeadCount();
+    List<Table> getTableList();
 }
