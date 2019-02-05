fb5291e20cf4d290a26df7938eb30f2ef02c06ce
Ernest Fraiman <upapaus@gmail.com>
2019-02-05 15:03:36 +0000

    Saving files before merge

---
+++ client/src/components/bookings/BookingForm.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const BookingForm = (props) => {
+  console.log(props);
+
+  const customerOptions = props.customer.map((customer) => {
+    return <option key={customer.id} value={customer._links.self.href}>{customer.lastName}</option>
+  })
+
+  const tableOptions = props.table.map((table) => {
+    return <option key={table.id} value={table._links.self.href}>{table.tableNumber}</option>
+  })
+
+  // const timeOptions = props.timeSlots.map(time, index) => {
+  //   return <option key={index} value={time}>{customer.lastName}</option>
+  // })
+
+  function handleSubmit(event){
+    event.preventDefault()
+    const booking = {
+      "date": event.target.date.value,
+      "time": event.target.time.value,
+      "headcount": event.target.headcount.value,
+      "customer": event.target.customer.value,
+      "table": event.target.table.value
+    }
+    props.handleBookingPost(booking)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <input type="date" placeholder="date" name="date"/>
+    <input type="string" placeholder="time" name="time"/>
+    <input type="number" placeholder="headcount" name="headcount"/>
+    <select name="customer">
+      {customerOptions}
+    </select>
+
+    <select name="table">
+      {tableOptions}
+    </select>
+
+    <button type="submit">Save</button>
+    </form>
+  )
+
+}
+
+export default BookingForm;
---
+++ client/src/containers/BookingFormContainer.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react'
+import Request from '../helpers/requestHelper.js';
+
+import BookingForm from '../components/bookings/BookingForm.js';
+
+class BookingFormContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      customers: [],
+      timeSlots: ["10:00", "11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00"],
+      tables: []
+    }
+    this.handleBookingPost = this.handleBookingPost.bind(this)
+  }
+
+  componentDidMount(){
+    const request = new Request()
+    request.get('/customers').then(data =>{
+      this.setState({customers: data._embedded.customers})
+    })
+
+    const request2 = new Request()
+    request2.get('/tables').then(data =>{
+      this.setState({tables: data._embedded.tables})
+    })
+  }
+
+  // componentDidMount(){
+  //   const request = new Request()
+  //   request.get('/tables').then(data =>{
+  //     this.setState({tables: data._embedded.tables})
+  //   })
+  // }
+
+  handleBookingPost(booking){
+    const request = new Request();
+    request.post('/bookings', booking).then(() => {
+      window.location = '/bookings'
+    })
+  }
+
+  handleBookingUpdate(booking){
+    const url = '/bookings/' + booking.id;
+    let request = new Request();
+    request.put(url, booking).then(data => {
+      window.location = '/bookings'
+    })
+  }
+
+  render(){
+    return (
+      <BookingForm customer={this.state.customers} table={this.state.tables} handleBookingPost={this.handleBookingPost} />
+    )
+  }
+
+}
+
+export default BookingFormContainer
