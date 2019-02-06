28f899c32e7be7118685ede19053cfe9ce746b76
Paul Wishart <misterwishart@gmail.com>
2019-02-06 12:58:46 +0000

    More work on receipts table

--- client/src/components/receipts/Receipt.js
+++ client/src/components/receipts/Receipt.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
 
 const Receipt = (props) => {
-  
+  return (
+    <tr>
+    <td>{props.booking.table.tableNumber}</td>
+    <td>{props.booking.date}</td>
+    <td>{props.booking.time}</td>
+    <td>{props.booking.headCount}</td>
+    <td>{props.booking.receiptItems.length}</td>
+    </tr>
+  );
 }
 
 export default Receipt;
---
+++ client/src/components/receipts/ReceiptsTableHeader.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+const ReceiptsTableHeader = () => {
+  return (
+    <thead>
+    <tr>
+    <th>Table Number</th>
+    <th>Date</th>
+    <th>Time</th>
+    <th>Size Of Party</th>
+    <th>No. of Receipt Items</th>
+    </tr>
+    </thead>
+  );
+}
+
+export default ReceiptsTableHeader;
