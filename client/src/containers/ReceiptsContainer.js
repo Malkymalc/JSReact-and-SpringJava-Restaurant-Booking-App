dcd9c7c3ace6f8868f7ecdcd1ee632e086592fac
Paul Wishart <misterwishart@gmail.com>
2019-02-06 11:53:24 +0000

    Started work on receipt items

--- client/src/containers/ReceiptsContainer.js
+++ client/src/containers/ReceiptsContainer.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Request from '../helpers/requestHelper.js';
+import ReceiptsList from '../components/receipts/ReceiptsList.js';
 
-const ReceiptsContainer = (props) => {
+export default class ReceiptsContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      customers: [],
+      currentCustomer: null
+    }
+
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  componentDidMount(){
+    let request = new Request()
+    request.get('customers').then(data => {
+      this.setState({ customers: data._embedded.customers});
+    })
+  }
+
+  handleSelect(event){
+    const customerID = event.target.value;
+    this.setState({currentCustomer: this.state.customers[customerID]});
+  }
+
+  render(){
+    const customerElements = this.state.customers.map((customer, index) => {
+      return <option key={customer.id} value={index}>{customer.firstName} {customer.lastName}</option>
+    })
 
   return (
-    <h1>This is receipts</h1>
-  );
+    <>
+    Select Customer:
+    <select onChange={this.handleSelect} name="customer">
+    {customerElements}
+    </select>
+    <ReceiptsList customer={this.state.currentCustomer}/>
+    </>
+  );}
 }
-
-export default ReceiptsContainer;
